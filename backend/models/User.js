f8645e18943ea39afd5963acb02183b7fe544b8e
cakpopascal29-ship-it <cakpopascal29@gmail.com>
2026-08-29 04:30:48 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  photoProfil: { type: String, default: '' },
+  status: { type: String, enum: ['actif', 'inactif'], default: 'inactif' },
+  lastSeen: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
